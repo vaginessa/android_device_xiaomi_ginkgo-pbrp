3aee284651c24b249d6d7e49db97f9da5ef7f6dd
erwinabs <erwinabubakarsidik@gmail.com>
2021-06-03 21:58:14 +0000

    ginkgo: add CRYPTO_METADATA

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -106,6 +106,7 @@ TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
 # Crypto
 TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_FBE := true
+TW_INCLUDE_FBE_METADATA_DECRYPT := true
 
 # TWRP specific build flags
 TW_THEME := portrait_hdpi
