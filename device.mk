2c1b1deaeb266f81d15b11060db2a3a05d1b69ad
Captain Throwback <captainthrowback@hotmail.com>
2022-05-21 10:31:06 -0400

    ginkgo: add custom asserts
    Change-Id: Ie7f0248affab1dae8ea28a8ecea16c6c9afc9ffe

--- device.mk
+++ device.mk
@@ -6,6 +6,9 @@ PRODUCT_PACKAGES_ENG += \
     qcom_decrypt \
     qcom_decrypt_fbe
 
+# Custom ROM asserts
+TARGET_OTA_ASSERT_DEVICE := ginkgo,willow
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
