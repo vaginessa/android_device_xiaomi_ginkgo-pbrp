034dde3ad8351e536001b3b31e5e8a23c16eee59
erwinabs <erwinabubakarsidik@gmail.com>
2022-10-03 15:27:25 +0000

    ginkgo: exclude python
    Change-Id: Ib3d69adb790fd0f0faca94ab0ac06c6d1c13a8b0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -147,6 +147,9 @@ TW_INCLUDE_RESETPROP := true
 TW_INCLUDE_REPACKTOOLS := true
 TW_EXCLUDE_NANO := true
 TW_EXCLUDE_BASH := true
+TW_EXCLUDE_PYTHON := true
+
+#Properties
 TW_OVERRIDE_SYSTEM_PROPS := \
      "ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
 
